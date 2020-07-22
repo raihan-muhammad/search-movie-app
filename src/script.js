8fbaaa2ec6d035b54de218284be6640c7ca6b92d
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-07-22 17:26:16 +0700

    init

---
+++ src/script.js
@@ -0,0 +1,34 @@
+const searchMovie = () => {
+  const input = document.getElementById("input-cari");
+  const result = document.getElementById("result");
+  const apiKey = "49cd9371";
+  const endPoint = `http://www.omdbapi.com/?apikey=${apiKey}`;
+  input.addEventListener("keyup", async () => {
+    try {
+      const req = await fetch(`${endPoint}&s=${input.value}`);
+      const res = await req.json();
+      res.Search.forEach((movie) => {
+        result.innerHTML = `
+              <div class="card mt-2">
+                  <img
+                  src="${movie.Poster}"
+                  class="card-img-top"
+                  alt="${movie.Title}"
+                  />
+                  <div class="card-body">
+                      <h5 class="card-title">${movie.Title}</h5>
+                      <p class="card-text">
+                      ${movie.Year}
+                      </p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+              </div>
+            `;
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+};
+
+searchMovie();
